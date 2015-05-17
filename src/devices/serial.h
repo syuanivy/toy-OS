566221e65bbf37a2bc12f78034b7f1a1181cb97d
hanjoes <hanzhou87@gmail.com>
2015-05-16 18:32:12 -0700

    Implemented backspace, minor tweak on shell.c. Also implemented non-busy waiting uart_putc

--- src/devices/serial.h
+++ src/devices/serial.h
@@ -3,6 +3,9 @@
 
 #include <stdint.h>
 
+#define CR 13
+#define BS 8
+
 void serial_init(void);
 void serial_putc (char);
 void serial_flush (void);
@@ -10,6 +13,9 @@ void serial_notify (void);
 
 void uart_putc(unsigned char byte);
 unsigned char uart_getc();
+void uart_write(const char* buffer, size_t size);
+void uart_puts(const char* str);
 void test_serial();
+void hexstrings(unsigned int d);
 
 #endif /* devices/serial.h */
