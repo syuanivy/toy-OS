20f4be3a66bdd3542a7d989a446c804e1830db05
rr118 <robert.p.reeves@gmail.com>
2015-05-17 20:51:08 -0700

    added examples

---
+++ src/misc/examples.h
@@ -0,0 +1,10 @@
+/*
+ * examples.h
+ *
+ *  Created on: May 15, 2015
+ *      Author: rreeves
+ */
+ 
+void loop(void *params);
+void priority_exec(void *params);
+void priority_takeover(void *params);
